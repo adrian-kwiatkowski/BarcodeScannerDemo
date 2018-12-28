b89456e24327819e8a67204dda303e36c8f6b9c5
Adrian Kwiatkowski <adrian.kwiatkowski@qpony.pl>
2018-12-28 14:46:48 +0100

    Initial Commit

---
+++ BarcodeScannerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeScannerDemo
+//
+//  Created by Adrian Kwiatkowski on 28/12/2018.
+//  Copyright © 2018 Adrian Kwiatkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
