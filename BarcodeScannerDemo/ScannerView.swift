ea8e0b8780bbf98d67b9687f6a85b0790beb758f
Adrian Kwiatkowski <adrian.kwiatkowski@qpony.pl>
2019-01-07 14:30:32 +0100

    separate scannerview to a class

---
+++ BarcodeScannerDemo/ScannerView.swift
@@ -0,0 +1,105 @@
+//
+//  ScannerView.swift
+//  BarcodeScannerDemo
+//
+//  Created by Adrian Kwiatkowski on 07/01/2019.
+//  Copyright © 2019 Adrian Kwiatkowski. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+final class ScannerView: UIView {
+    
+    private var captureSession: AVCaptureSession!
+    private var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    convenience init(frame: CGRect, scanArea: CGRect) {
+        self.init(frame: frame)
+        
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13]
+            
+        } else {
+            failed()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = frame
+        previewLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+        
+        metadataOutput.rectOfInterest = previewLayer.metadataOutputRectConverted(fromLayerRect: scanArea)
+    }
+    
+    private override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func failed() {
+        print("Scanning not supported. Your device does not support scanning a code from an item. Please use a device with a camera.")
+        captureSession = nil
+    }
+    
+    func startRunning() {
+        if captureSession.isRunning == false {
+            captureSession.startRunning()
+        }
+    }
+    
+    func stopRunning() {
+        if captureSession.isRunning == true {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func found(code: String) {
+        print(code)
+    }
+}
+
+// MARK: - AVCaptureMetadataOutputObjectsDelegate
+extension ScannerView: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        captureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+    }
+}
--- BarcodeScannerDemo/ScannerViewController.swift
+++ BarcodeScannerDemo/ScannerViewController.swift
@@ -3,91 +3,47 @@
 import AVFoundation
 import UIKit
 
-class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
-    var captureSession: AVCaptureSession!
-    var previewLayer: AVCaptureVideoPreviewLayer!
+class ScannerViewController: UIViewController {
+    
+    private var scannerView: ScannerView?
+    private var overlayView: UIView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.black
-        captureSession = AVCaptureSession()
         
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
-        let videoInput: AVCaptureDeviceInput
+        let x = view.frame.minX + 30
+        let y = view.frame.maxY / 3
+        let width = view.frame.width - 60
+        let height = view.frame.height / 3
+        let scanArea = CGRect(x: x, y: y, width: width, height: height)
         
-        do {
-            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {
-            return
-        }
+        scannerView = ScannerView(frame: view.layer.bounds, scanArea: scanArea)
+        view.addSubview(scannerView!)
         
-        if (captureSession.canAddInput(videoInput)) {
-            captureSession.addInput(videoInput)
-        } else {
-            failed()
-            return
-        }
+        let testView = UIView(frame: scanArea)
+        testView.layer.borderWidth = 1
+        testView.layer.borderColor = UIColor.white.cgColor
+        view.addSubview(testView)
         
-        let metadataOutput = AVCaptureMetadataOutput()
-        
-        if (captureSession.canAddOutput(metadataOutput)) {
-            captureSession.addOutput(metadataOutput)
-            
-            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
-        } else {
-            failed()
-            return
-        }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = view.layer.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        
-        captureSession.startRunning()
-    }
-    
-    func failed() {
-        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
-        captureSession = nil
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        if (captureSession?.isRunning == false) {
-            captureSession.startRunning()
-        }
+        scannerView?.startRunning()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        if (captureSession?.isRunning == true) {
-            captureSession.stopRunning()
-        }
+        scannerView?.stopRunning()
     }
-    
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        captureSession.stopRunning()
-        
-        if let metadataObject = metadataObjects.first {
-            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-            guard let stringValue = readableObject.stringValue else { return }
-            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-            found(code: stringValue)
-        }
-        
-        dismiss(animated: true)
-    }
-    
-    func found(code: String) {
-        print(code)
-    }
+}
+
+// MARK: - Styling
+extension ScannerViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
